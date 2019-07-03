237d9c41e81b03523b83e21d3e2892a8eebda600
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-07-03 17:43:14 -0300

    Have made a little change in the 'SecurityConfig' class to give permission to the other HTTP methods, so it don't return a 'Invalid CORS Request' error

--- src/main/java/com/rodrigo/integration/config/SecurityConfig.java
+++ src/main/java/com/rodrigo/integration/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.rodrigo.integration.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final String[] PUBLIC_MATECHERS = { 
+	private static final String[] PUBLIC_MATCHERS = { 
 			"/notes/**", 
 			"/users/**"
 	};
@@ -25,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();	
 		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATECHERS).permitAll()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -33,8 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", config);
 		return source;
 	}
 
